683e4a4b1839e26e6b77333d09e0f5d901fcba4a
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2022-02-10 11:08:23 +0100

    added HttpClientBuilderWithoutDisableConnectionState rule and unit tests

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/remoting/HttpClientBuilderWithoutDisableConnectionStateTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.remoting;
+
+import net.sourceforge.pmd.testframework.PmdRuleTst;
+
+public class HttpClientBuilderWithoutDisableConnectionStateTest extends PmdRuleTst {
+}
