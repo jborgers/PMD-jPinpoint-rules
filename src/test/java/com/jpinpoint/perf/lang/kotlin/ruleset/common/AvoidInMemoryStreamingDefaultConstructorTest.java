a17ee72adbdc523f22f0903e1fafcc14e965e527
jborgers <jborgers@jpinpoint.com>
2024-02-09 18:49:44 +0100

    Convert applicable rules to Kotlin #185 - AvoidInMemoryStreamingDefaultConstructor - (kotlin unit tests fail to run)

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/common/AvoidInMemoryStreamingDefaultConstructorTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.common;
+
+import net.sourceforge.pmd.testframework.PmdRuleTst;
+
+public class AvoidInMemoryStreamingDefaultConstructorTest extends PmdRuleTst {
+}
