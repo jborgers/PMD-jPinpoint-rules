65ac2af385a45640b5b6212f52e5d859a939494e
jborgers <jborgers@jpinpoint.com>
2025-12-23 17:09:41 +0100

    Rule request (Kotlin): configure Netty ConnectionProvider explicitly: NettyConnectionProviderWithoutMaxOrTimeout #552

---
+++ src/test/java/com/jpinpoint/perf/lang/kotlin/ruleset/remoting/NettyConnectionProviderWithoutMaxOrTimeoutTest.java
@@ -0,0 +1,6 @@
+package com.jpinpoint.perf.lang.kotlin.ruleset.remoting;
+
+import net.sourceforge.pmd.test.PmdRuleTst;
+
+public class NettyConnectionProviderWithoutMaxOrTimeoutTest extends PmdRuleTst {
+}
